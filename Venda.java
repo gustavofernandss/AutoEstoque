256a4ee729747d12b4a4a56a1b677017797ab733
gustavofernandss <87822363+gustavofernandss@users.noreply.github.com>
2023-08-08 00:10:07 -0300

    Update Venda.java

--- Venda.java
+++ Venda.java
@@ -4,90 +4,111 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Venda {
-	
-	private int idVenda;
-	private float valor;
-	private OperadorCaixa operador;
-	private List<Produto> produto;
-	private LocalDate dataVenda = LocalDate.now();
-	private LocalTime horaVenda = LocalTime.now();
-	
-	
-	public int getIdVenda() {
-		return idVenda;
-	}
-	public void setIdVenda(int idVenda) {
-		if(idVenda >= 0) {
-		this.idVenda = idVenda;}
-		else {
-		throw new IllegalArgumentException("ID de venda inválido");
-		}
-	}
-	public float getValor() {
-		return valor;
-	}
-	public void setValor(float valor) {
-		if(valor >= 0) {
-		this.valor = valor;}
-		else {
-		throw new IllegalArgumentException("valor inválido");
-		
-	}
-	public OperadorCaixa getOperador() {
-		return operador;
-	}
-	public void setOperador(OperadorCaixa operador) {
-		if(operador != 0) {
-		this.operador = operador;}
-		else {
-		throw new IllegalArgumentException("Operador inválido");
-		
-	}
-	public List<Produto> getProduto() {
-		return produto;
-	}
-	public void setProduto(Produto[] produto) {
-		if(produto != 0) {
-		this.produto = produto;}
-		else {
-		throw new IllegalArgumentException("Produto inválido");
-		
-	}
-	public LocalDate getDataVenda() {
-		return dataVenda;
-	}
-	public void setDataVenda(LocalDate dataVenda) {
-		if(produto != 0) {
-		this.produto = produto;}
-		else {
-		throw new IllegalArgumentException("data inválida");
-		this.dataVenda = dataVenda;
-	}
-	public LocalTime getHoraVenda() {
-		return horaVenda;
-	}
-	public void setHoraVenda(LocalTime horaVenda) {
-		if(produto != 0) {
-		this.produto = produto;}
-		else {
-		throw new IllegalArgumentException("hora inválido");
-		this.horaVenda = horaVenda;
-	}
 
-	
-	
-	public void realizarVenda() {}
-	public void exibirVenda() {}
-	public void removerProduto(List<Produto> produto) {}
-	public void adicionarProduto(List<Produto> produto) {}
+    private int idVenda;
+    private float valor;
+    private OperadorCaixa operador;
+    private List<Produto> produtos;  // Corrigido o nome da lista para 'produtos'
+    private LocalDate dataVenda = LocalDate.now();
+    private LocalTime horaVenda = LocalTime.now();
 
-	
-	//+ realizarVenda()
-	//+ exibirVenda() 
-	//+ removerProduto(produto[]: Produto) + adicionarProduto(produto[]: Produto)
+    public int getIdVenda() {
+        return idVenda;
+    }
 
+    public void setIdVenda(int idVenda) {
+        if (idVenda >= 0) {
+            this.idVenda = idVenda;
+        } else {
+            throw new IllegalArgumentException("ID de venda inválido");
+        }
+    }
 
-	
-	
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        if (valor >= 0) {
+            this.valor = valor;
+        } else {
+            throw new IllegalArgumentException("valor inválido");
+        }
+    }
+
+    public OperadorCaixa getOperador() {
+        return operador;
+    }
+
+    public void setOperador(OperadorCaixa operador) {
+        if (operador != null) {  // Corrigido o teste de nulidade
+            this.operador = operador;
+        } else {
+            throw new IllegalArgumentException("Operador inválido");
+        }
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {  // Corrigido o nome do parâmetro
+        if (produtos != null) {
+            this.produtos = produtos;
+        } else {
+            throw new IllegalArgumentException("Produtos inválidos");
+        }
+    }
+
+    public LocalDate getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(LocalDate dataVenda) {
+        if (dataVenda != null) {
+            this.dataVenda = dataVenda;
+        } else {
+            throw new IllegalArgumentException("Data inválida");
+        }
+    }
+
+    public LocalTime getHoraVenda() {
+        return horaVenda;
+    }
+
+    public void setHoraVenda(LocalTime horaVenda) {
+        if (horaVenda != null) {
+            this.horaVenda = horaVenda;
+        } else {
+            throw new IllegalArgumentException("Hora inválida");
+        }
+    }
+
+    public void realizarVenda() {
+        System.out.println("Venda realizada");
+    }
+
+    public void exibirVenda() {
+        System.out.println("ID da Venda: " + getIdVenda());
+        System.out.println("Valor da Venda: " + getValor());
+        System.out.println("Operador: " + getOperador());
+
+        System.out.println("Produtos:");
+        for (Produto produto : getProdutos()) {
+            System.out.println(produto);
+        }
+
+        System.out.println("Data da Venda: " + getDataVenda());
+        System.out.println("Hora da Venda: " + getHoraVenda());
+    }
+
+    public void removerProduto(Produto produto) {
+        produtos.remove(produto);
+        System.out.println("Produto removido");
+    }
+
+    public void adicionarProduto(Produto produto) {
+        produtos.add(produto);
+        System.out.println("Produto adicionado");
+    }
 }
-
