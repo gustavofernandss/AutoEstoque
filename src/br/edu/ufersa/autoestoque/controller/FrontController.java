839594d242f5faa6737238093a2a7f44baca6dbe
Sueliton Silva <102476188+Sueliton85s@users.noreply.github.com>
2023-10-12 07:51:11 -0300

    Add files via upload

---
+++ src/br/edu/ufersa/autoestoque/controller/FrontController.java
@@ -0,0 +1,14 @@
+package br.edu.ufersa.autoestoque.controller;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class FrontController {
+	@FXML private TextField email;  
+	@FXML private TextField senha;
+	@FXML private Label erroAut;
+	public void autenticar(ActionEvent event) {
+		
+	}
+}
---
+++ src/br/edu/ufersa/autoestoque/controller/Gerente.java
@@ -0,0 +1,60 @@
+package br.edu.ufersa.autoestoque.controller;
+
+public class Gerente {
+	private String nome;
+	private String senha;
+	private int idGerente;
+	private boolean isGerente;
+	
+	public Gerente(String nome, String senha, boolean isGerente, int idGerente){
+		setNome(nome);
+		setSenha(senha);
+		setIsGerente(isGerente);
+		setIdGerente(idGerente);
+	}
+	 
+	public void setNome(String nome){
+		if(nome != null){
+			this.nome = nome;
+		}else{
+			System.out.println("O nome não pode ser vazio");
+		}
+	}
+	
+	public void setSenha(String senha){
+			if(senha != null){
+				this.senha = senha;
+			}else{
+				System.out.println("A senha não pode ser vazio");
+			}
+	}
+	
+	public int getIdGerente() {
+		return idGerente;
+	}
+
+	public void setIdGerente(int idGerente) {
+		this.idGerente = idGerente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setIsGerente(boolean isGerente) {
+		this.isGerente = isGerente;
+	}
+
+	public boolean getIsGerente(){
+		return isGerente;
+	}
+	
+	public boolean autenticar(String nome, String senha){
+		return this.nome.equals(nome) && this.senha.equals(senha);
+		
+	}
+}
