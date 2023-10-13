191f9f5b9e72638627d75b334f3db4603d3082f5
andrade-paulo <andrade.oluap@gmail.com>
2023-10-13 15:54:02 -0300

    Testing DAO

--- src/br/edu/ufersa/autoestoque/AutoEstoque.java
+++ src/br/edu/ufersa/autoestoque/AutoEstoque.java
@@ -35,5 +35,49 @@ public class AutoEstoque {
 		} catch (SQLException e) {
 			System.out.println("Erro ao listar usuários: " + e.getMessage());
 		}
+
+		// Atualizar usuário
+		try {
+			usuario.setNome("Ciclano");
+			usuario.setSenha("9876543210");
+			usuarioDAO.atualizar(usuario);
+			System.out.println("Usuário atualizado com sucesso!");
+		} catch (Exception e) {
+			System.out.println("Erro ao atualizar usuário: " + e.getMessage());
+		}
+
+		// Listar usuario por CPF
+		try {
+			ResultSet rs = usuarioDAO.buscarPorId(usuario);
+			while (rs.next()) {
+				System.out.println("CPF: " + rs.getString("CPF"));
+				System.out.println("Nome: " + rs.getString("nome"));
+				System.out.println("Senha: " + rs.getString("senha"));
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro ao listar usuário por CPF: " + e.getMessage());
+		}
+
+		// Deletar usuário
+		try {
+			usuarioDAO.deletar(usuario);
+			System.out.println("Usuário deletado com sucesso!");
+		} catch (Exception e) {
+			System.out.println("Erro ao deletar usuário: " + e.getMessage());
+		}
+
+		System.out.println("");
+
+		// Listar usuários
+		try {
+			ResultSet rs = usuarioDAO.listar();
+			while (rs.next()) {
+				System.out.println("CPF: " + rs.getString("CPF"));
+				System.out.println("Nome: " + rs.getString("nome"));
+				System.out.println("Senha: " + rs.getString("senha"));
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro ao listar usuários: " + e.getMessage());
+		}
 	}
 }
